ec64b54d57aa14c8762202f5914c828053a9ff13
root <Victor-kay>
2023-12-01 18:36:46 -0800

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -8,4 +8,5 @@ file_line { 'Turn off passwd auth':
 file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?\\s*IdentityFile\\s+~/.ssh/school',
 }
